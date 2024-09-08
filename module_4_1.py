fd93b7bf52aebf4d67e426297962fca96c251bf6
IRNorthman <ir.northman@gmail.com>
2024-09-09 00:48:51 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as fm
+import true_math as tm
+
+result1 = fm.divine(69, 3)
+result2 = fm.divine(3, 0)
+result3 = tm.divine(49, 7)
+result4 = tm.divine(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,5 @@
+def divine(first, second):
+    if second != 0:
+        return(first/second)
+    else:
+        return 'Ошибка'
